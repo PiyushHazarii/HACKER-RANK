9bdfb504348e367c9cc52514806f2567751a7e50
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-06-12 19:30:17 +0530

    Solution in java

---
+++  Java_Stdin_and_Stdout_I.java
@@ -0,0 +1,36 @@
+package Hacker Rank;
+
+
+import java.util.*;
+ 
+
+public class  Java_Stdin_and_Stdout_I 
+{
+
+    public static void main(String[] args) 
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt();
+
+        // Complete this line
+        // Complete this line
+
+        int b = sc.nextInt();
+
+        int c = sc.nextInt();
+
+        System.out.println(a);
+
+        System.out.println(b);
+
+        // Complete this line
+        // Complete this line
+
+
+        System.out.println(c);
+        
+    }
+}
+
