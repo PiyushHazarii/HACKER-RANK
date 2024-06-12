57431f44f57272bb86e84fb52c79457039552046
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-06-12 20:42:50 +0530

    Solution in java

---
+++ Java_Primality_Test.java
@@ -0,0 +1,49 @@
+package Hacker Rank;
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class Java_Primality_Test 
+{
+    public static void main(String[] args) throws IOException 
+    {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+         try 
+         {
+                String n = bufferedReader.readLine();
+                BigInteger number = new BigInteger(n);
+        
+            if(number.isProbablePrime(1))
+            {
+                System.out.println("prime");
+            }
+
+            else
+            {
+                System.out.println("not prime");
+            } 
+        }
+            catch(IOException e)
+            {
+                System.out.println("Could not validate input");
+            }
+
+        bufferedReader.close();
+    }
+}
+
+    
+
