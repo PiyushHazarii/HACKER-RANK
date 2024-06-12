3743c617bdd4e47e7ea455de5ec9a6012902dc43
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-06-12 20:31:29 +0530

    Solution in java

---
+++ Java_Regex.java
@@ -0,0 +1,11 @@
+package Hacker Rank;
+
+
+
+public class Java_Regex 
+{
+        String zeroTo255 = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
+
+        String pattern = zeroTo255+"."+zeroTo255+"."+zeroTo255+"."+zeroTo255;
+
+}    
