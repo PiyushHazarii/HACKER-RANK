90183b70c7f41bd7267d3cdb81aff148f73a171e
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-06-12 19:26:22 +0530

    Solution in java

---
+++ Welcome_to_Java.java
@@ -0,0 +1,19 @@
+package Hacker Rank;
+
+public class Welcome_to_Java {
+
+        public static void main(String[] args)
+        {
+
+            /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+
+
+            System.out.println("Hello, World.");
+
+            System.out.print("Hello, Java.");
+
+        }
+    }
+    
+    
+
