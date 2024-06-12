6d8bd9dd289959ab32620d80039f52501c9b5f42
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-06-12 21:54:57 +0530

    Solution in java

---
+++ Java_BigInteger.java
@@ -0,0 +1,26 @@
+package Hacker Rank;
+
+
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+
+public class Java_BigInteger 
+{
+
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+
+       BigInteger a1= sc.nextBigInteger();
+
+       BigInteger b1 = sc.nextBigInteger();
+
+       System.out.println(a1.add(b1));
+
+       System.out.println(a1.multiply(b1));
+    }
+}
+    
+
