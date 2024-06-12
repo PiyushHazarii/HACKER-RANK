c6a86f05741e0d0e25c4c3a0fb8a8eaad7a2c0de
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-06-12 19:44:34 +0530

    Solution in java

---
+++ Java_Output_Formatting.java
@@ -0,0 +1,38 @@
+package Hacker Rank;
+
+
+import java.util.Scanner;
+
+public class Java_Output_Formatting 
+{
+
+    public static void main(String[] args) 
+    {
+
+            Scanner sc=new Scanner(System.in);
+
+            System.out.println("================================");
+
+            for(int i=0;i<3;i++)
+            {
+
+                String s=sc.next();
+
+                int x=sc.nextInt();
+
+                //Complete this line
+
+                System.out.printf("%-15s%03d\n",s,x);
+
+            }
+
+            System.out.println("================================");
+
+    }
+}
+
+
+
+
+    
+
