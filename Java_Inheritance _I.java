a600f01efed15929a17916b095740f0bcd0f9070
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-06-13 11:12:43 +0530

    Solution in java

---
+++ Java_Inheritance _I.java
@@ -0,0 +1,19 @@
+package Hacker Rank;
+
+
+class Bird extends Animal
+{
+	void fly()
+	{
+		System.out.println("I am flying");
+	}
+
+    void sing()
+    {
+        System.out.println("I am singing");
+    }
+}
+
+
+    
+
