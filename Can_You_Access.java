6cae0acacedb1b9655b35762124295d399f64cc3
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-06-13 12:54:51 +0530

    Solution in java

---
+++ Can_You_Access.java
@@ -0,0 +1,11 @@
+package Hacker Rank;
+
+public class Can_You_Access 
+{
+
+    o = new Inner().new Private();
+
+    System.out.println(num+" is "+((Solution.Inner.Private)o).powerof2(num));
+
+    
+}
