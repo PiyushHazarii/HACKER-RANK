e3ef13da0e3dc1b87691829861804c16ab7ac5ef
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-06-13 12:13:40 +0530

    Solution in java

---
+++ Java_Method.java
@@ -0,0 +1,9 @@
+package Hacker Rank;
+
+public class Java_Method-Overriding {
+
+    void getNumberOfTeamMembers()
+    {
+        System.out.println("Each team has 11 players in Soccer Class");
+    }   
+}
