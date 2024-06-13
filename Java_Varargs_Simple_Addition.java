f9a58c16c994c2520489add1a7277cc7c36b073a
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-06-13 12:51:36 +0530

    Solution in java

---
+++ Java_Varargs_Simple_Addition.java
@@ -0,0 +1,28 @@
+package Hacker Rank;
+
+public class Java_Varargs_Simple_Addition {
+
+class Add
+{
+   void  add(int n1 ,int n2 )
+    {
+         System.out.println(n1+"+"+n2+"="+(n1+n2)); 
+    }
+
+    void  add(int n1 ,int n2,int n3)
+    {
+         System.out.println(n1+"+"+n2+"+"+n3+"="+(n1+n2+n3));
+    }
+
+    void  add(int n1,int n2,int n3 , int n4 ,int n5)
+    {
+         System.out.println( n1+"+"+n2+"+"+n3+"+"+n4+"+"+n5+"="+(n1+n2+n3+n4+n5));
+    }
+
+   void  add(int n1 ,int n2,int n3 , int n4 ,int n5,int n6)
+    {
+          System.out.println(n1+"+"+n2+"+"+n3+"+"+n4+"+"+n5+"+"+n6+"="+(n1+n2+n3+n4+n5+n6));
+    }
+
+}   
+}
