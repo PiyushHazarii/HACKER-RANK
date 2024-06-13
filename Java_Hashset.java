030e831cb47834f75bbc8c1f94f6131e068c9ef1
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-06-13 10:56:06 +0530

    Solution in java

---
+++ Java_Hashset.java
@@ -0,0 +1,13 @@
+package Hacker Rank;
+
+public class Java_Hashset {
+
+    HashSet<String> hs = new HashSet<>();
+
+    for(int i=0;i<t;i++)
+    {
+        hs.add( pair_left[i]+" "+ pair_right[i]);
+
+        System.out.println(hs.size());
+    }
+}
