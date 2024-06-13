8456130222a0ea9796e422b02e2344a6b3bcce3e
mepiyushhazari@gmail.in <163896832+PiyushHazarii@users.noreply.github.com>
2024-06-13 10:57:37 +0530

    Solution in java

---
+++ Java_Generics.java
@@ -0,0 +1,28 @@
+package Hacker Rank;
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+public class Java_Generics 
+{
+
+    public static void main(String[] args) 
+    {
+       System.out.println("1");
+
+       System.out.println("2");
+
+       System.out.println("3");
+
+       System.out.println("Hello");
+
+       System.out.println("World");
+
+    }   
+}
+
+    
+
